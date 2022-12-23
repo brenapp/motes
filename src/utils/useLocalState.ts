4df94990bd98be62744cc78b0f2d9f6b5d807a9a
Brendan McGuire <brendan@bren.app>
2022-12-23 01:41:53 -0600

    Enable use in SSR

--- src/utils/useLocalState.ts
+++ src/utils/useLocalState.ts
@@ -3,22 +3,22 @@ import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
 export function useLocalStorage<T>(identifier: string, initial: T): [T, Dispatch<SetStateAction<T>>] {
 
-    const [state, setState] = useState<T>(() => {
-
-        if (typeof window === 'undefined') {
-            return initial;
-        }
-
-        const value = localStorage.getItem(identifier);
-        if (value) {
-            return JSON.parse(value);
-        }
-        return initial;
-    });
+    const [state, setState] = useState<T>(initial);
+    const [hydrated, setHydrated] = useState(false);
 
     useEffect(() => {
-        localStorage.setItem(identifier, JSON.stringify(state));
-    }, [state])
+        const value = localStorage.getItem(identifier);
+        if (value) {
+            setState(JSON.parse(value));
+        }
+        setHydrated(true);
+    }, []);
+
+    useEffect(() => {
+        if (hydrated) {
+            localStorage.setItem(identifier, JSON.stringify(state));
+        }
+    }, [state, hydrated])
 
     return [state, setState];
 }
@@ -26,17 +26,21 @@ export function useLocalStorage<T>(identifier: string, initial: T): [T, Dispatch
 export function useSessionStorage<T>(identifier: string, initial: T): [T, Dispatch<SetStateAction<T>>] {
 
     const [state, setState] = useState<T>(initial);
+    const [hydrated, setHydrated] = useState(false);
 
     useEffect(() => {
         const value = sessionStorage.getItem(identifier);
         if (value) {
             setState(JSON.parse(value));
         }
-    }, [])
+        setHydrated(true);
+    }, []);
 
     useEffect(() => {
-        sessionStorage.setItem(identifier, JSON.stringify(state));
-    }, [state])
+        if (!hydrated) {
+            sessionStorage.setItem(identifier, JSON.stringify(state));
+        }
+    }, [state, hydrated])
 
     return [state, setState];
 }
