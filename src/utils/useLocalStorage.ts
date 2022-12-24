05e559423e6600d0fb84b4018b3000b83034d3eb
Brendan McGuire <brendan@bren.app>
2022-12-23 23:59:12 -0600

    Type imports

--- src/utils/useLocalStorage.ts
+++ src/utils/useLocalStorage.ts
@@ -1,9 +1,9 @@
-import React, { useDebugValue, useEffect, useState } from "react"
+import { type Dispatch, type SetStateAction, useDebugValue, useEffect, useState } from "react"
 
 export const useLocalStorage = <S>(
     key: string,
     initialState?: S | (() => S)
-): [S, React.Dispatch<React.SetStateAction<S>>] => {
+): [S, Dispatch<SetStateAction<S>>] => {
     const [state, setState] = useState<S>(initialState as S)
     useDebugValue(state)
 
